82ff18c54850f8860df7d25fb5b95b7ae0299be7
Johnny Melo <Johnnymelowork@gmail.com>
2019-01-15 23:28:37 -0500

    webpage layout with a navbar

--- src/App.js
+++ src/App.js
@@ -7,8 +7,52 @@ import './App.css';
 
 class App extends Component {
   state = {
-    skill
-  } 
+    skill,
+    currentScore: 0,
+    topScore: 0,
+    winLoss: "",
+
+      // Add to Array if Add Click is true
+    clicked: []
+  };
+
+  handleClick = id => {
+    console.log("button clicked");
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    }
+  };
+
+  handleIncrement = () => {
+
+    const newScore = this.state.currentScore + 1;
+    console.log("Increment counter " + newScore);
+    this.setState({
+      currentScore: newScore,
+    });
+    if (newScore === 12) {
+      this.setState({ winLoss: "Congrats! Play some more?" });
+      this.toggleModal()
+      if (newScore >= this.state.topScore) {
+        this.setState({ topScore: newScore });
+      }
+    } else {
+      this.handleShuffle();
+    }
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      winLoss: "You lose, try again!",
+      clicked: [],
+    });
+    if (this.state.currentScore >= this.state.topScore) {
+      this.setState({ topScore: this.state.currentScore });
+    }
+  }
 
   render() {
     return (
