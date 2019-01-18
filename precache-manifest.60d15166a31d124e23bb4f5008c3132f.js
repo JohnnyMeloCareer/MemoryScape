2b2f6f0f6f904039041405d0a8a02a5a1c9d1596
Johnny Melo <Johnnymelowork@gmail.com>
2019-01-18 10:50:50 -0500

    Updates

---
+++ precache-manifest.60d15166a31d124e23bb4f5008c3132f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "070d672e75b48dd0ab4d",
+    "url": "/React-Click-Scape/static/js/runtime~main.070d672e.js"
+  },
+  {
+    "revision": "420bd04ed29ce59a444c",
+    "url": "/React-Click-Scape/static/js/main.420bd04e.chunk.js"
+  },
+  {
+    "revision": "5410354e271753cd670f",
+    "url": "/React-Click-Scape/static/js/1.5410354e.chunk.js"
+  },
+  {
+    "revision": "420bd04ed29ce59a444c",
+    "url": "/React-Click-Scape/static/css/main.4888ac1a.chunk.css"
+  },
+  {
+    "revision": "f57116919d22bbe4084f4ff0393e10f4",
+    "url": "/React-Click-Scape/index.html"
+  }
+];
